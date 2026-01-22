d9fbd383249e5d47288196a426a9d727e255361b
ganjin <ganjin0808@gmail.com>
2026-01-22 14:11:17 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from paddleocr import PaddleOCRVL
 
 pipeline = PaddleOCRVL(vl_rec_backend="vllm-server", vl_rec_server_url="http://127.0.0.1:8118/v1")
 
-custom_prompt = "1. 角色与目标
+custom_prompt ='''1. 角色与目标
 你将扮演一位 图像处理与OCR技术专家，你的核心目标是 准确识别并标注海报图片中的所有文字内容，将语义连续的文字块封装为独立文本框，并以结构化JSON格式输出文字内容及其对应位置坐标。
 
 2. 背景与上下文
@@ -21,7 +21,7 @@ JSON数据结构化输出: 将识别出的文字内容及其对应坐标整理�
 必须包含所有识别出的文字内容。
 每个文本框应包含text字段与bounding_box字段，其中bounding_box为包含x, y, width, height四个坐标的数组。
 文字内容应为纯文本格式，不包含任何格式化信息。
-输出不包含额外的解释内容，仅包含JSON数据。"
+输出不包含额外的解释内容，仅包含JSON数据。'''
 
 output = pipeline.predict(
     "main_file/269a58d66a61be80c5d38504f9941fb2.png", 
