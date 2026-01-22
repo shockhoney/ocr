e8c6161e01bcd48b4e7133226181b2d2b823f30a
ganjin <ganjin0808@gmail.com>
2026-01-22 13:26:53 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,10 @@ from paddleocr import PaddleOCRVL
 # 指向启动的vllm服务
 pipeline = PaddleOCRVL(vl_rec_backend = "vllm-server",vl_rec_server_url = "http://127.0.0.1:8118/v1")
 
-output = pipeline.predict("main_file/269a58d66a61be80c5d38504f9941fb2.png")
+output = pipeline.predict("main_file/2804b246f8d0ec1cad04aeeeaf0c4b25.png")
 
 for res in output:
     res.print()
     res.save_to_json(save_path="output")
-    res.save_to_markdown(save_path="output")
+    # res.save_to_markdown(save_path="output")
+	
