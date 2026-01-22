366e861dbda0a850104b2233dd5f8a2de24e38d0
ganjin <ganjin0808@gmail.com>
2026-01-22 10:59:20 +0800

    Update snapshot_download to include model name and cache dir

--- download.py
+++ download.py
@@ -1,3 +1,4 @@
 from modelscope import snapshot_download
 
-model_dir = snapshot_download(
+model_dir = snapshot_download('PaddlePaddle/PaddleOCR-VL',cache_dir = 
+"/home/devops/works/paddleocr_vl")
